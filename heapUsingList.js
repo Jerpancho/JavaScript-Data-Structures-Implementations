36015230e61bd75869fcd04395acbfc3abbfb159
Jerpancho <panchojerrold@gmail.com>
2022-09-01 14:07:11 -0500

    added remove max heap method

--- heapUsingList.js
+++ heapUsingList.js
@@ -22,6 +22,49 @@ class MaxHeap {
     }
     return this.heap;
   }
+  // remove the head of the heap and down-heap the replaced root;
+  extractMax() {
+    // swap the max num with the end of the list
+    let max = this.heap[0];
+    let endHeap = this.heap.pop();
+    if (this.heap.length > 0) {
+      this.heap[0] = endHeap;
+      // perform down-heap procedure
+      this.sinkDown();
+    }
+
+    return max;
+  }
+  sinkDown() {
+    let index = 0;
+    let value = this.heap[0];
+    const length = this.heap.length;
+
+    while (true) {
+      let leftIndex = 2 * index + 1;
+      let rightIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftIndex < length) {
+        leftChild = this.heap[leftIndex];
+        if (leftChild > value) {
+          swap = leftIndex;
+        }
+      }
+      if (rightIndex < length) {
+        rightChild = this.heap[rightIndex];
+        if (swap === null && rightChild > value || swap !== null && rightChild > leftChild) {
+          swap = rightIndex;
+        }
+      }
+
+      if (swap === null) break;
+      this.heap[index] = this.heap[swap];
+      this.heap[swap] = value;
+      index = swap;
+    }
+  }
 }
 
 let heap = new MaxHeap();
@@ -37,3 +80,4 @@ heap.insert(42);
 heap.insert(84);
 heap.insert(12);
 console.log(heap.insert(31));
+console.log(heap.extractMax());
